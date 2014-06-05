8376829baee5823897280ae5ecaa0a9fbeac4e3a
angeloocana <angeloocana@gmail.com>
2014-06-04 22:21:44 -0300

    Inicio Projeto

---
+++ DonatellaPG/DonatellaPG/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DonatellaPG.Startup))]
+namespace DonatellaPG
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
